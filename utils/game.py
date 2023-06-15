97dd76e5dda2cdc83f2bb58d897b019b56b090fb
Muhammad Firdausyi <muhammad.anhar.firdausyi@gmail.com>
2023-06-15 21:03:51 +0200

    UPDATE 11 - updating game.py This update is an attempt to address code readability issue, as well as an attempt to pre-empt potential bugs

--- utils/game.py
+++ utils/game.py
@@ -8,46 +8,45 @@ Last Update:
 """
 
 
-"""
-Importing libraries
-"""
+# Importing libraries
+
 import string as str
 import random 
 
 """
-defining class Hangman
-Has a class attribute: 
+Defining class Hangman
+Has a class attribute: Hangman.LETTER_TO_USE
 There are 5 methods enlisted within the class, with the purpose to start the game upon calling the 'star_game' method
 """
 class Hangman:
-    # class attributes
+    
     LETTERS_TO_USE = list(str.ascii_lowercase)
-
+        # class attributes
     def __init__(self):
         self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
         self.word_to_find = random.choice(self.possible_words)
         self.turn_count = 0
         self.error_count = 0
-        self.duplicate_loc = []
-        self.to_look = 0
-    
-        ## to enhance user experience and to make it easier to draw conclusion of the game, these following attributes are defined
         self.correctly_guessed_letters = ["_" for _ in self.word_to_find] 
         self.wrongly_guessed_letters = []
         self.lives = 5
+    
+        ## to enhance user experience and to make it easier to draw conclusion of the game, these following attributes are defined
+        self.duplicate_loc = []
+        self.to_look = 0
         
         
     # defining method for find locations of duplicate letters in a self.word_to_find
     def find_duplicate_letter(self, letter):
         self.duplicate_loc = [i for i, l in enumerate(self.word_to_find) if l == letter]
         return self.duplicate_loc
-
+    
     # defining Hangman.play() method to outline the inner working of the game
     def play(self, guess):
         if guess in Hangman.LETTERS_TO_USE: 
             if guess in self.word_to_find:
 
-                ### to address situations when self.word_to_find contains duplicate letters
+                #### to address situations when self.word_to_find contains duplicate letters
                 if self.word_to_find.count(guess) != 1:
                     for loc in self.find_duplicate_letter(guess):
                         self.correctly_guessed_letters[loc] = guess
@@ -56,7 +55,14 @@ class Hangman:
                     self.correctly_guessed_letters[self.word_to_find.index(guess)] = guess
                     self.turn_count += 1
 
-            elif guess not in self.word_to_find:
+            ### to address situations when user input a previously used letter
+            elif guess in self.wrongly_guessed_letters or guess in self.correctly_guessed_letters:
+                if guess != "_":
+                    self.error_count += 1
+                    self.turn_count += 1
+                    print("You have used that letter before. Think hard and try again!")
+
+            else:
                 self.lives -= 1
                 self.turn_count += 1
                 self.wrongly_guessed_letters.append(guess)
@@ -64,7 +70,7 @@ class Hangman:
             self.error_count += 1
             self.turn_count += 1
             print("Your input is not a lowercase letter. Please try again!")
-        
+
     # defining Hangman.game_over() method to display a string when the player loses the game
     def game_over(self):
         print("Game over...")
@@ -89,6 +95,7 @@ You have found the word: \'{self.word_to_find}\' in {self.turn_count} turns with
                 new_game = Hangman()
                 new_game.start_game()
                 break
+
             elif inquiry == "n":
                 print("""
 -------------------------------------------------------------------------------        
@@ -96,6 +103,7 @@ Thank you for playing! Have a nice day!
 ------------------------------------------------------------------------------- 
         """)
                 break
+
             else:
                 print("Invalid input. Please try again.")
     
